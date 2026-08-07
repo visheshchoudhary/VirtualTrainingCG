f6851c052ff2a11c7e14d6287258932b80dc1720
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2026-08-07 10:16:18 +0530

    Update Student.java

--- Mapping/Student.java
+++ Mapping/Student.java
@@ -1,47 +1,51 @@
-package entity;
+package mapping;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 
 @Entity
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     private String name;
 
+    private String course;
+
     @ManyToOne
+    @JoinColumn(name = "dept_id")
     private Department department;
 
-    public Student() {
-    }
-
-    public Student(String name){
-        this.name=name;
-    }
-
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id){
-        this.id=id;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name){
-        this.name=name;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
     }
 
     public Department getDepartment() {
         return department;
     }
 
-    public void setDepartment(Department department){
-        this.department=department;
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 }
