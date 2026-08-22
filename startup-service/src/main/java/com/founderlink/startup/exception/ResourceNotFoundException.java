1a62bbd9a9ec22d0262b7e7e8dd17b50ac1423bb
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2026-08-22 10:09:24 +0530

    Delete startup-service directory

--- startup-service/src/main/java/com/founderlink/startup/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.startup.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
