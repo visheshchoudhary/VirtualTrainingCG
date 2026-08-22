0558143fc770ecb3536be7eb7e2a6392d73a4a62
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2026-08-22 10:08:04 +0530

    Delete auth-service directory

--- auth-service/src/main/java/com/founderlink/auth/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.auth.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
