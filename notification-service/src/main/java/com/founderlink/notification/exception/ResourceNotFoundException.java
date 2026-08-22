c6689853abb56758c7187291c452af0c3e6f5fb7
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2026-08-22 10:09:00 +0530

    Delete notification-service directory

--- notification-service/src/main/java/com/founderlink/notification/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.notification.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
