d9e054fc36467b495539c85ef67dec2fcb5674b1
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2026-08-22 10:08:51 +0530

    Delete messaging-service directory

--- messaging-service/src/main/java/com/founderlink/messaging/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.messaging.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
