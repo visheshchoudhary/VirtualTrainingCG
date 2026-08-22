5c149a7e0b708c1d0d5eee6c2ef93df158839c90
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2026-08-22 10:09:10 +0530

    Delete payment-service directory

--- payment-service/src/main/java/com/founderlink/payment/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.payment.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
--- payment-service/src/main/java/com/founderlink/payment/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.payment.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
