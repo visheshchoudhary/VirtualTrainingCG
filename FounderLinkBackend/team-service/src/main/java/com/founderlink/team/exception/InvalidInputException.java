4c4e1b23956f3e466d21c0571d30e1ebe56fbb7c
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2026-08-22 15:22:16 +0530

    Delete FounderLinkBackend/team-service directory

--- FounderLinkBackend/team-service/src/main/java/com/founderlink/team/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.team.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
--- FounderLinkBackend/team-service/src/main/java/com/founderlink/team/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.team.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
