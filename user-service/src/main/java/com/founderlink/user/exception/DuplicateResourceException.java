6d96b9432f80657bcbe45c0856907fd758717774
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2026-08-22 10:09:50 +0530

    Delete user-service directory

--- user-service/src/main/java/com/founderlink/user/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.founderlink.user.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
